a19efb16ca0b5ac74722e83e6bdde87e2292ea4b
Brooks Lybrand <brookslybrand@gmail.com>
2025-12-02 10:43:49 -0600

    React Router 7.10.0 Upgrade (#15)
    * Upgrade deps and enable v8 flags
    
    * Fix types
    
    * Update @react-router dependencies to version 7.10.0 in package.json and package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig, splitVendorChunkPlugin } from "vite";
+import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import arraybuffer from "vite-plugin-arraybuffer";
 
 export default defineConfig({
   build: {
@@ -11,10 +10,5 @@ export default defineConfig({
     noExternal: ["@docsearch/react"],
   },
   optimizeDeps: { exclude: ["svg2img"] },
-  plugins: [
-    tsconfigPaths(),
-    splitVendorChunkPlugin(),
-    arraybuffer(),
-    reactRouter(),
-  ],
+  plugins: [tsconfigPaths(), reactRouter()],
 });
